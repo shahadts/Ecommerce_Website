7721dbf8b248cded09c94f385f9ca5d8acc8ffbe
Shahad <188241172+shahadts@users.noreply.github.com>
2026-03-11 00:55:50 +0300

    Update settings.py

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -17,11 +17,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ts1gql=lh3o4t&07f+-k_+*63h0^j(!iri39$q2ei%8+$tg38!'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
